89f463024663117995560fc8fd569c3b18bde03e
島倉健麿 <>
2024-05-12 17:53:54 +0900

    VPC作成

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # terraform
 個人練習用リポジトリ
+
+## terraformコマンドメモ
+terraform fmt
+terraform plan
+terraform apply
+terraform apply -auto-approve
+terraform destroy
--- main.tf
+++ main.tf
@@ -1,19 +1,35 @@
-provider "aws" {
-    profile = "terraform"
-    region = "ap-northeast-1"
-}
-
-resource "aws_instance" "hello_world" {
-  ami = "ami-0ab3794db9457b60a"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "HelloWorldTag"
+## conf
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
+    }
   }
-
-  user_data = <<EOF
-#!/bin/bash
-amazon-linux-extras install -y nginx1.12
-systemctl start nginx
-EOF
 }
+
+## provider
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
+
+## variables
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+## EC2 AMI
+# resource "aws_instance" "hello_world" {
+#   ami           = "ami-0ab3794db9457b60a"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "HelloWorldTag"
+#   }
+# }
